34c4f625d2c34940fe4a68e037efd437eb379567
Christian Landgren <christian@landgren.nu>
2024-06-10 01:16:24 +0200

    fix: add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
-
+import node from '@astrojs/node'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [react(), tailwind()],
+  adapter: node({
+    mode: 'standalone',
+  }),
 })
