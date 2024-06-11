766f1d57d37befa9380702b80f538427646e85a9
Elvira <elvbom@users.noreply.github.com>
2024-06-11 15:09:54 +0200

    minor styling adjustments

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,17 +9,17 @@ export function Header() {
   ]
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container relative flex h-14 max-w-screen-2xl items-center justify-between">
+    <header className="sticky top-0 z-50 w-full border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container relative flex h-12 max-w-screen-2xl items-center justify-between">
         <div className="absolute inset-0 flex justify-center items-center">
           <a className="flex items-center space-x-2" href="https://www.klimatkollen.se/">
             <img src='/klimatkollen_logo.svg' alt='Klimatkollen logga' width={150} />
           </a>
         </div>
         <div className="flex flex-1 items-center justify-end space-x-2">
-          <nav className="flex items-center gap-4 text-sm lg:gap-6">
+          <nav className="flex items-center gap-4 text-m lg:gap-5">
             {navItems.map(item => (
-              <a key={item.href} className="transition-colors hover:text-foreground/80 text-foreground/60" href={item.href}>
+              <a key={item.href} className="text-white cursor-pointer hover:underline" href={item.href}>
                 {item.label}
               </a>
             ))}
