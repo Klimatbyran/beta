a5491b86b9c38a4610b9878399470ebab9f8e301
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-06-11 18:59:54 +0200

    Format

--- astro.config.js
+++ astro.config.js
@@ -6,14 +6,20 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  server: process.env.NODE_ENV === 'production' ? { port: parseInt(process.env.PORT) || 4321, host: true } : undefined,
+  server:
+    process.env.NODE_ENV === 'production'
+      ? { port: parseInt(process.env.PORT) || 4321, host: true }
+      : undefined,
   integrations: [react(), tailwind()],
   adapter: node({
     mode: 'standalone',
   }),
-  // NOTE: Temporary redirect (HTTP 302) to reduce risk of broken links.
-  redirects: process.env.NODE_ENV === 'production' ? {
-    '/': 'https://klimatkollen.se',
-    '/foretag': 'https://klimatkollen.se',
-  }: undefined,
+  // NOTE: Temporary redirect (HTTP 302) to reduce risk of broken links once we implement these URLs as part of the Astro site.
+  redirects:
+    process.env.NODE_ENV === 'production'
+      ? {
+          '/': 'https://klimatkollen.se',
+          '/foretag': 'https://klimatkollen.se',
+        }
+      : undefined,
 })
