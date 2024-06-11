1af411089bfa9c2cfa6d10916df3b7d06070fe36
Elvira <elvbom@users.noreply.github.com>
2024-06-11 15:02:54 +0200

    center logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export function Header() {
   const navItems = [
     { href: 'https://www.klimatkollen.se/kallor-och-metod', label: 'Källor och metod' },
@@ -8,13 +10,13 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center justify-center md:justify-between">
-        <div className="hidden md:flex flex-1 justify-center md:justify-center">
+      <div className="container relative flex h-14 max-w-screen-2xl items-center justify-between">
+        <div className="absolute inset-0 flex justify-center items-center">
           <a className="flex items-center space-x-2" href="https://www.klimatkollen.se/">
             <img src='/klimatkollen_logo.svg' alt='Klimatkollen logga' width={150} />
           </a>
         </div>
-        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+        <div className="flex flex-1 items-center justify-end space-x-2">
           <nav className="flex items-center gap-4 text-sm lg:gap-6">
             {navItems.map(item => (
               <a key={item.href} className="transition-colors hover:text-foreground/80 text-foreground/60" href={item.href}>
