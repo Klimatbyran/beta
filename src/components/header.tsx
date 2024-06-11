b2a7f0217e42b55784b04b954af33d684e3c5bf3
Elvira <elvbom@users.noreply.github.com>
2024-06-11 15:01:42 +0200

    rm span

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,12 +8,10 @@ export function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
-        <div className="mr-4 hidden md:flex">
-          <a className="mr-6 flex items-center space-x-2" href="https://www.klimatkollen.se/">
-            <span className="hidden font-bold sm:inline-block text-2xl">
-              <img src='/klimatkollen_logo.svg' alt='Klimatkollen logga' width={150} />
-            </span>
+      <div className="container flex h-14 max-w-screen-2xl items-center justify-center md:justify-between">
+        <div className="hidden md:flex flex-1 justify-center md:justify-center">
+          <a className="flex items-center space-x-2" href="https://www.klimatkollen.se/">
+            <img src='/klimatkollen_logo.svg' alt='Klimatkollen logga' width={150} />
           </a>
         </div>
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
