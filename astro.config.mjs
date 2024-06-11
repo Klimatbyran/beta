7965ed13ed250da3ef2ecfabc16f1bcc73e0c76f
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-06-11 17:15:53 +0200

    Add redirects and avoid exposing to network during development unless passed explicitly as CLI flag

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,14 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  server: { port: parseInt(process.env.PORT) || 4321, host: true },
+  server: process.env.NODE_ENV === 'production' ? { port: parseInt(process.env.PORT) || 4321, host: true } : undefined,
   integrations: [react(), tailwind()],
   adapter: node({
     mode: 'standalone',
   }),
+  // NOTE: Temporary redirect (HTTP 302) to reduce risk of broken links.
+  redirects: process.env.NODE_ENV === 'production' ? {
+    '/': 'https://klimatkollen.se',
+    '/foretag': 'https://klimatkollen.se',
+  }: undefined,
 })
