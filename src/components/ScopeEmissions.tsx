7e57916a3b83e96475796a405f85b203fe4e5a2c
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-06-12 15:29:52 +0200

    Prevent scope emissions from crashing the page

--- src/components/ScopeEmissions.tsx
+++ src/components/ScopeEmissions.tsx
@@ -16,21 +16,23 @@ export function ScopeEmissions({ title, emissions }: ScopeEmissionsProps) {
     <div className="grid gap-4">
       <div className="font-semibold">{title}</div>
       <div className="grid gap-4">
-        {emissions.map((emission, index) => (
-          <div key={index} className="flex items-center justify-between">
-            <div>
-              <div className="font-medium">{emission.subtitle}</div>
-              <div className="text-sm text-gray-400 dark:text-gray-500">
-                {emission.description}
+        {emissions.map((emission, index) =>
+          emission.value ? (
+            <div key={index} className="flex items-center justify-between">
+              <div>
+                <div className="font-medium">{emission.subtitle}</div>
+                <div className="text-sm text-gray-400 dark:text-gray-500">
+                  {emission.description}
+                </div>
+              </div>
+              <div className="flex items-center gap-2 text-2xl font-bold">
+                <div className="rounded-full bg-gray-800 p-2 dark:bg-gray-800">
+                  {emission.value.toLocaleString('sv-se')}
+                </div>
               </div>
             </div>
-            <div className="flex items-center gap-2 text-2xl font-bold">
-              <div className="rounded-full bg-gray-800 p-2 dark:bg-gray-800">
-                {emission.value.toLocaleString('sv-se')}
-              </div>
-            </div>
-          </div>
-        ))}
+          ) : null,
+        )}
       </div>
     </div>
   )
