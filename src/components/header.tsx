55a0ea4cf4c65668d5741b7d010a15f124e628df
Elvira <elvbom@users.noreply.github.com>
2024-06-12 09:08:42 +0200

    remove links due to issues w hamburger menu and time constraints, instead an arrow to go back to legacy site

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,23 +9,23 @@ export function Header() {
   ]
 
   return (
-    <header className="sticky top-0 z-50 w-full border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-gray-300 bg-black bg-opacity-95 backdrop-blur supports-[backdrop-filter]:bg-opacity-60">
       <div className="container relative flex h-12 max-w-screen-2xl items-center justify-between">
-        <div className="absolute inset-0 flex justify-center items-center">
-          <a className="flex items-center space-x-2" href="https://www.klimatkollen.se/">
-            <img src='/klimatkollen_logo.svg' alt='Klimatkollen logga' width={150} />
+        <div className="absolute left-0 flex items-center h-full pl-4 z-30">
+          <a href="https://www.klimatkollen.se/foretag/utslappen/lista" className="text-white" title="Go back">
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
+            </svg>
           </a>
         </div>
-        <div className="flex flex-1 items-center justify-end space-x-2">
-          <nav className="flex items-center gap-4 text-m lg:gap-5">
-            {navItems.map(item => (
-              <a key={item.href} className="text-white cursor-pointer hover:underline" href={item.href}>
-                {item.label}
-              </a>
-            ))}
-          </nav>
+        <div className="absolute inset-0 flex justify-center items-center z-20">
+          <a className="flex items-center space-x-2" href="https://www.klimatkollen.se/">
+            <img src="/klimatkollen_logo.svg" alt="Klimatkollen logga" width={150} />
+          </a>
         </div>
       </div>
     </header>
   )
 }
+
+export default Header
