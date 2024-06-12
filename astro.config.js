03f30fd45b6777d172d88588b9131d497a2c52f3
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-06-12 12:09:35 +0200

    Only redirect in production

--- astro.config.js
+++ astro.config.js
@@ -16,8 +16,9 @@ export default defineConfig({
     mode: 'standalone',
   }),
   // NOTE: Temporary redirect (HTTP 302) to reduce risk of broken links once we implement these URLs as part of the Astro site.
+  // This should only happen when running in the docker environment (which is the only time we pass in an explicit PORT env variable).
   redirects:
-    process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV === 'production' && parseInt(process.env.PORT)
       ? {
           '/': 'https://klimatkollen.se',
           '/foretag': 'https://klimatkollen.se',
