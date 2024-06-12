40c0932e82b42ac260dc2a5b3d3202373d6bb56b
Elvira <elvbom@users.noreply.github.com>
2024-06-12 09:31:25 +0200

    cleanup and basic styling of footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,38 +1,21 @@
+import React from 'react'
 import { Card } from './ui/card'
 
 export function Footer() {
   return (
-    <Card>
-      <footer className="text-center mt-10">
-        <p className="text-lg mb-2">Kontakta oss</p>
-        <p className="text-5xl mb-4">hej@klimatkollen.se</p>
-        <button className="px-4 py-2 mb-4 bg-gray-700 rounded-md text-white">
-          Kopiera mailadress
-        </button>
-        <div className="flex justify-center space-x-4 mb-4">
-          <a href="#">
-            <i className="fab fa-github"></i>
-          </a>
-          <a href="#">
-            <i className="fab fa-linkedin"></i>
-          </a>
-          <a href="#">
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a href="#">
-            <i className="fab fa-discord"></i>
-          </a>
-        </div>
-        <p className="text-sm text-gray-400 mb-2">
-          Klimatkollen är en medborgarplattform som tillgängliggör klimatdata
-          och är utvecklad med öppen källkod.
-        </p>
-        <div className="flex justify-center space-x-4 text-gray-400 text-sm">
-          <a href="#">Privacy & Terms</a>
-          <a href="#">International license</a>
-          <a href="#">CC BY-SA - Attribution-ShareAlike 4.0</a>
+    <Card className="bg-black text-white">
+      <footer className="text-left mt-10 p-6">
+        <div className="max-w-4xl ml-auto">
+          <p className="text-sm mb-2">Klimatkollen är en medborgarplattform som tillgängliggör klimatdata</p>
+          <p className="text-sm text-white-400 mb-4">
+            CC BY-SA <a href='http://creativecommons.org/licenses/by-sa/4.0/' className="underline">Attribution-ShareAlike 4.0 International license</a><br />
+            Klimatkollen är utvecklad med <a href='https://github.com/Klimatbyran/klimatkollen' className="underline">öppen källkod</a> och drivs av den ideella föreningen Klimatbyrån.
+          </p>
+          <img src="/klimatkollen_logo.svg" alt="Klimatkollen logotyp" className="mb-4" width={250} />
         </div>
       </footer>
     </Card>
   )
 }
+
+export default Footer
