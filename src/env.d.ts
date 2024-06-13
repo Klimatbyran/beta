f43b4e36c6e0d573aa80fd88b0f4481714d83aed
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-06-13 19:18:46 +0200

    Add support for using icons in both Svelte and Astro components

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare module 'icons:astro/*' {
+  const component: (props: astroHTML.JSX.SVGAttributes) => astroHTML.JSX.Element
+  export default component
+}
+
+declare module 'icons:svelte/*' {
+  import { SvelteComponent } from 'svelte'
+  import type { SvelteHTMLElements } from 'svelte/elements'
+  export default class extends SvelteComponent<SvelteHTMLElements['svg']> {}
+}
