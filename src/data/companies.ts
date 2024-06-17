b774e6b308d1a58d69c0336a85c4fe256c6331b0
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-06-17 21:26:50 +0200

    Add draft for getCompany() and load all companies via simple memory cache

---
+++ src/data/companies.ts
@@ -0,0 +1,62 @@
+import { createCache } from '@/lib/createCache'
+import type { CompanyData } from './companyData'
+
+const cachedCompanies = createCache<'companies', CompanyData[]>({
+  maxAge: 5 * 60 * 1000,
+})
+
+/**
+ * Conditionally load cached local JSON file instead of fetching from the API
+ */
+export async function getCompanies() {
+  const cached = cachedCompanies.get('companies')
+  if (cached) {
+    return cached
+  }
+
+  let data: CompanyData[]
+
+  if (process.env.NODE_ENV === 'production') {
+    data = (await fetch('https://api.klimatkollen.se/api/companies').then(
+      (res) => res.json(),
+    )) as CompanyData[]
+
+    cachedCompanies.set('companies', data)
+  } else {
+    // prettier-ignore
+    // @ts-ignore Don't care about this TS error
+    data = (await import('@/data/test.json')).default as CompanyData[]
+  }
+
+  return data
+
+  // TODO: Move filtering to the backend.
+  // return (
+  //   data
+  //     .filter(isCompany)
+  //     // HACK: Keep unique companies. This should ideally be solved with filtering on the API side.
+  //     .filter(
+  //       (c, i, array) =>
+  //         i === array.findLastIndex((company) => c.url === company.url),
+  //     ) as CompanyData[]
+  // )
+}
+
+// /**
+//  * NOTE: If we need this in the future, here's a suggestion for how to implement it
+//  */
+// export async function getCompany(wikidataId: string) {
+//   let data: CompanyData
+
+//   if (process.env.NODE_ENV === 'production') {
+//     data = (await fetch(
+//       `https://api.klimatkollen.se/api/companies/${wikidataId}`,
+//     ).then((res) => res.json())) as CompanyData
+//   } else {
+//     // prettier-ignore
+//     // @ts-ignore Don't care about this TS error
+//     data = ((await import('@/data/test.json')).default as CompanyData[]).find(c => c.wikidataId.toLowerCase() === wikidataId.toLowerCase())
+//   }
+
+//   return data
+// }
