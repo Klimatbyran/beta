498298a24b195648fa1e684cde6dce7e8048de67
Christian Landgren <christian@landgren.nu>
2024-06-19 00:22:21 +0200

    feat: fetch single company

--- src/data/companies.ts
+++ src/data/companies.ts
@@ -19,21 +19,21 @@ export async function getCompanies() {
   return data
 }
 
-// /**
-//  * NOTE: If we need this in the future, here's a suggestion for how to implement it
-//  */
-// export async function getCompany(wikidataId: string) {
-//   let data: CompanyData
+/**
+ * NOTE: If we need this in the future, here's a suggestion for how to implement it
+ */
+export async function getCompany(wikidataId: string) {
+  let data: CompanyData
 
-//   if (process.env.NODE_ENV === 'production') {
-//     data = (await fetch(
-//       `https://api.klimatkollen.se/api/companies/${wikidataId}`,
-//     ).then((res) => res.json())) as CompanyData
-//   } else {
-//     // prettier-ignore
-//     // @ts-ignore Don't care about this TS error
-//     data = ((await import('@/data/test.json')).default as CompanyData[]).find(c => c.wikidataId.toLowerCase() === wikidataId.toLowerCase())
-//   }
+  if (process.env.NODE_ENV === 'production') {
+    data = (await fetch(
+      `https://api.klimatkollen.se/api/companies/${wikidataId}`,
+    ).then((res) => res.json())) as CompanyData
+  } else {
+    // prettier-ignore
+    // @ts-ignore Don't care about this TS error
+    data = ((await import('@/data/test.json')).default as CompanyData[]).find(c => c.wikidataId.toLowerCase() === wikidataId.toLowerCase())
+  }
 
-//   return data
-// }
+  return data
+}
