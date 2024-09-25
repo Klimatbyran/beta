2ae52958355794bccd77167bada4bf4a5ca477ce
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-09-25 19:07:08 +0200

    Create minimal fetch API wrapper

---
+++ src/lib/request.ts
@@ -0,0 +1,31 @@
+const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000/api'
+    : 'https://api.klimatkollen.se/api'
+
+export async function request(
+  endpoint: string,
+  { body, ...customConfig }: RequestInit = {},
+) {
+  const headers = { 'Content-Type': 'application/json' }
+  const config: RequestInit = {
+    method: body ? 'POST' : 'GET',
+    ...customConfig,
+    headers: {
+      ...headers,
+      ...customConfig.headers,
+    },
+  }
+  if (body) {
+    config.body = JSON.stringify(body)
+  }
+
+  return fetch(`${BASE_URL}${endpoint}`, config).then(async (response) => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      const errorMessage = await response.text()
+      return Promise.reject(new Error(errorMessage))
+    }
+  })
+}
