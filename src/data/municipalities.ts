68679d10129533d5f72285892d1a5c079be08590
Elvira <elvbom@users.noreply.github.com>
2024-11-16 17:38:17 +0100

    add municipality data service

---
+++ src/data/municipalities.ts
@@ -0,0 +1,95 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as fs from 'fs'
+import * as path from 'path'
+import type {
+  Municipality,
+  EmissionPerYear,
+  Budget,
+  Emission,
+  Trend,
+  ClimatePlan,
+  ApproximatedEmission,
+} from './municipalityTypes'
+import jsonData from './municipalityData.json'
+
+export class MunicipalityDataService {
+  public getMunicipality(name: string): Municipality {
+    const data = jsonData.find(
+      (m: any) => m.kommun.toLowerCase() === name.toLowerCase(),
+    )
+    if (!data) {
+      throw new Error(`Municipality with name "${name}" not found`)
+    }
+
+    return this.processMunicipalityData(data)
+  }
+
+  private processMunicipalityData(data: any): Municipality {
+    const emissions = Object.entries(data.emissions).map(
+      ([year, emission]) =>
+        ({
+          Year: Number(year),
+          CO2Equivalent: emission,
+        }) as EmissionPerYear,
+    )
+
+    const emission: Emission = {
+      EmissionPerYear: emissions,
+      LargestEmissionSectors: [], // Process this if available
+      HistoricalEmissionChangePercent: data.historicalEmissionChangePercent,
+    }
+
+    const approximatedEmission: ApproximatedEmission = {
+      TotalCO2Emission: data.totalApproximatedHistoricalEmission,
+      EmissionPerYear: Object.entries(data.approximatedHistoricalEmission).map(
+        ([year, co2equivalent]) => ({
+          Year: Number(year),
+          CO2Equivalent: Number(co2equivalent),
+        }),
+      ),
+    }
+
+    const trend: Trend = {
+      TrendCO2Emission: data.trendEmission,
+      TrendPerYear: Object.entries(data.trend).map(([year, emissionTrend]) => ({
+        Year: Number(year),
+        CO2Equivalent: Number(emissionTrend),
+      })),
+    }
+
+    const budget: Budget = {
+      CO2Equivalent: data.budget,
+      BudgetPerYear: Object.entries(data.emissionBudget).map(
+        ([year, emissionBudget]) => ({
+          Year: Number(year),
+          CO2Equivalent: Number(emissionBudget),
+        }),
+      ),
+    }
+
+    const climatePlan: ClimatePlan = {
+      Link: data.climatePlanLink,
+      YearAdapted: data.climatePlanYear,
+      Comment: data.climatePlanComment,
+    }
+
+    return {
+      Name: data.kommun,
+      HistoricalEmission: emission,
+      ApproximatedHistoricalEmission: approximatedEmission,
+      EmissionTrend: trend,
+      Budget: budget,
+      NeededEmissionChangePercent: data.neededEmissionChangePercent,
+      HitNetZero: data.hitNetZero,
+      BudgetRunsOut: data.budgetRunsOut,
+      ElectricCarChangePercent: data.electricCarChangePercent,
+      ElectricCarChangeYearly: data.electricCarChangeYearly,
+      ClimatePlan: climatePlan,
+      BicycleMetrePerCapita: data.bicycleMetrePerCapita,
+      TotalConsumptionEmission: data.totalConsumptionEmission / 1000,
+      ElectricVehiclePerChargePoints: data.electricVehiclePerChargePoints,
+      ProcurementScore: data.procurementScore,
+      ProcurementLink: data.procurementLink,
+    } as Municipality
+  }
+}
---
+++ src/data/municipalityTypes.ts
@@ -0,0 +1,69 @@
+export type Image = {
+  ImageUrl: string
+  Description: string
+}
+
+export type EmissionPerYear = {
+  Year: number
+  CO2Equivalent: number
+}
+
+export type EmissionSector = {
+  Name: string
+  Year: string
+  CO2Equivalent: number
+  SubSectors: Array<EmissionSector>
+}
+
+export type Emission = {
+  EmissionPerYear: Array<EmissionPerYear>
+  LargestEmissionSectors: Array<EmissionSector>
+  HistoricalEmissionChangePercent: number
+  HistoricalEmissionChangeRank?: number | null
+}
+
+export type ApproximatedEmission = {
+  EmissionPerYear: Array<EmissionPerYear>
+  TotalCO2Emission: number | null
+}
+
+export type Budget = {
+  CO2Equivalent: number
+  BudgetPerYear: Array<EmissionPerYear>
+}
+
+export type Trend = {
+  TrendPerYear: Array<EmissionPerYear>
+  TrendCO2Emission: number
+}
+
+export type ClimatePlan = {
+  Link: string
+  YearAdapted: string
+  Comment: string
+}
+
+export type Municipality = {
+  County: string
+  Name: string
+  CoatOfArmsImage: Image | null
+  Population: number | null
+  Image: Image | null
+  Budget: Budget
+  HistoricalEmission: Emission
+  PoliticalRule: Array<string> | null
+  ApproximatedHistoricalEmission: ApproximatedEmission
+  EmissionTrend: Trend
+  NeededEmissionChangePercent: number
+  HitNetZero: number | string
+  BudgetRunsOut: string
+  ElectricCars: number
+  ElectricCarChangePercent: number
+  ElectricCarChangeYearly: Array<number>
+  ClimatePlan: ClimatePlan
+  BicycleMetrePerCapita: number
+  TotalConsumptionEmission: number
+  ElectricVehiclePerChargePoints: number
+  ProcurementScore: number
+  ProcurementLink: string
+}
