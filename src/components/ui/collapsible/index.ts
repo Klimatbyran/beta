0e4d048e014746c2cdd73ab5d60328c1d7f50976
Zara <74423786+ZaraBaghdasaryan@users.noreply.github.com>
2024-12-02 17:39:52 +0100

    Imported accordion from Svelte, adjusted the design and readjusted the folder/file structure

--- src/components/ui/collapsible/index.ts
+++ src/components/ui/collapsible/index.ts
@@ -1,15 +1,19 @@
-import { Collapsible as CollapsiblePrimitive } from "bits-ui";
-import Content from "./collapsible-content.svelte";
+import { Collapsible as CollapsiblePrimitive } from 'bits-ui'
+import Content from './collapsible-content.svelte'
+import Button from './button.svelte'
 
-const Root = CollapsiblePrimitive.Root;
-const Trigger = CollapsiblePrimitive.Trigger;
+const Root = CollapsiblePrimitive.Root
+const Trigger = CollapsiblePrimitive.Trigger
+const CollapsibleButton = Button // Export your Button here
 
 export {
-	Root,
-	Content,
-	Trigger,
-	//
-	Root as Collapsible,
-	Content as CollapsibleContent,
-	Trigger as CollapsibleTrigger,
-};
+  Root,
+  Content,
+  Trigger,
+  CollapsibleButton as Button, // Export the button here
+
+  //
+  Root as Collapsible,
+  Content as CollapsibleContent,
+  Trigger as CollapsibleTrigger,
+}
