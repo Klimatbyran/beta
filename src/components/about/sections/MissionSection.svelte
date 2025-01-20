9f8ceffa95cd1654449f2a4b6757183d39117c43
Kayla Woodbury <kaylawoodbury@mac.lan>
2025-01-20 16:28:54 +0100

    feat/create mission, funding and project section

---
+++ src/components/about/sections/MissionSection.svelte
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<div>
+  <p>
+    Klimatkollen är en medborgarplattform som hjälper medborgare, företag och
+    organisationer att få koll på klimatomställningen, genom att samla in och
+    visualisera klimatdata.\n\nVi visar hur det går med utsläppen, jämfört med
+    hur det borde gå, så att det blir begripligt för allmänheten, bidrar till en
+    faktabaserad debatt och stärker opinionen för klimatåtgärder.\n\nVår vision
+    är att beslutsfattare i politik och näringsliv ska påverkas av en
+    välinformerad och växande opinion till att genomföra åtgärder som sänker
+    utsläppen i linje med Parisavtalet. \n\nKlimatkollen lanserades i mars 2022
+    och drivs av den ideella föreningen Klimatbyrån. Initiativtagare är Ola
+    Spännar och Frida Berry Eklund, båda med lång erfarenhet inom kommunikation
+    och opinionsbildning.\n\nVi började med lokal klimatdata för Sveriges
+    kommuner. Under 2024 presenteras klimatdata även för
+    företag.\n\nKlimatkollen är utvecklad med öppen källkod. Det betyder att
+    alla kan vara med och utveckla och förbättra sajten via vårt
+    [Github-repos](https://github.com/Klimatbyran/).\n\nLäs mer om varför
+    Klimatkollen behövs i vår
+    [debattartikel](https://www.aktuellhallbarhet.se/alla-nyheter/debatt/darfor-behovs-klimatkollen/)
+    här.
+  </p>
+</div>
---
+++ src/components/about/sections/ProjectSection.svelte
@@ -0,0 +1,26 @@
+<script>
+</script>
+
+<div>
+  <p>
+    Under 2023 fick Klimatkollen stöd av
+    [Postkodstiftelsen](https://postkodstiftelsen.se/) för att ta tempen på hur
+    det går med klimatomställningen i kommunerna. I dag kan du se hur det går
+    med koldioxidutsläppen. Nu visar vi även vad kommunerna gör för att minska
+    dem. Vi utgår ifrån de största utsläppssektorerna och visar sedan hur det
+    går med viktiga omställningar inom dessa sektorer, genom att presentera
+    statistik om utvalda nyckeltal, ett nyckeltal per omställning.\n\nInför
+    riksdagsvalet 2022 ansvarade Klimatkollen för två unika projekt för att ge
+    medborgare bättre koll på utsläppseffekterna av partiernas
+    klimatpolitik:\n\n1. Klimatmål\nEn [analys](/partierna) av
+    riksdagspartiernas klimatmål, tillsammans med forskarnätverket Researchers’
+    Desk, Världsnaturfonden WWF, Våra barns klimat och ClimateView, i samarbete
+    med PwC och Naturskyddsföreningen. Analysen visade att sex av åtta partier
+    missar Parisavtalets 1,5-gradersmål.\n2.
+    [Utsläppsberäkning](/utslappsberakningar) av riksdagspartiernas politik
+    gällande tolv centrala klimatåtgärder. Bakom uträkningarna står
+    Klimatkollen, Världsnaturfonden WWF, ClimateView, Naturskyddsföreningen och
+    Våra barns klimat. Beräkningarna visade att den nya regeringens politik kan
+    öka utsläppen med 25 miljoner ton redan under mandatperioden 2022–2026.
+  </p>
+</div>
