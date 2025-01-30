69563a86d492054a20eeecf70fe80a147758a746
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-01-30 10:22:47 +0100

    Make API config to work both with Node.js scripts and Astro
    Co-authored-by: Kayla Woodbury <kaylawoodbury@users.noreply.github.com>

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,6 @@
 export default {
-  BASE_URL: import.meta.env.DEV
-    ? 'http://localhost:3000/api'
-    : 'https://api.klimatkollen.se/api',
+  BASE_URL:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000/api'
+      : 'https://api.klimatkollen.se/api',
 }
