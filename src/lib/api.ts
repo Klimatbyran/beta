81069679f218bc4c6208a9a5f15e6bdef892f2bb
Christian Landgren <christian@landgren.nu>
2025-02-03 09:50:35 +0100

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import createClient from 'openapi-fetch'
 import type { paths } from './api-types'
 
-const baseUrl = 'https://stage-api.klimatkollen.se'
+const baseUrl = 'https://stage-api.klimatkollen.se/api'
 const { GET } = createClient<paths>({ baseUrl })
 
 // Cache configuration
