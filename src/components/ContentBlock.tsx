de0f5b73f7852af3b60b43849a2a2c06b5a0d120
Elvira <elvbom@users.noreply.github.com>
2025-02-18 14:41:34 +0100

    minor restyle of font sizes in content block

--- src/components/ContentBlock.tsx
+++ src/components/ContentBlock.tsx
@@ -18,7 +18,7 @@ export function ContentBlock({ title, content, className }: ContentBlockProps) {
       <div className="space-y-4 md:space-y-8">
         <div className="flex items-center gap-2 md:gap-4">
           <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-[#E2FF8D]" />
-          <Text className="text-grey text-lg md:text-3xl">{title}</Text>
+          <Text className="text-grey text-md md:text-2xl">{title}</Text>
         </div>
 
         <Text className="max-w-full md:max-w-[90%] text-lg md:text-4xl leading-[1.2] md:leading-[1.1] tracking-[-0.02em] break-words hyphens-auto">
