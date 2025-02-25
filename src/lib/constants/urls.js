afcfe726d3411f608964379c51ff5a54d3d18632
Elvira <elvbom@users.noreply.github.com>
2025-02-25 12:57:36 +0100

    correct sp mistake

--- src/lib/constants/urls.js
+++ src/lib/constants/urls.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL =
   process.env.NODE_ENV === "production"
     ? "https://api.klimatkollen.se"
-    : process.env.STAGE_ENV === "staging"
+    : process.env.NODE_ENV === "staging"
     ? "https://stage.api.klimatkollen.se"
     : "http://localhost:3000";
